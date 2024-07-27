04164a525ccc7d33f33b8548aa146a56123a63b0
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-07-27 16:19:52 +0100

    chore: change bg-colour for `NewsFeed` & `Clock` component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,7 +1,7 @@
 const Clock = () => {
   return (
     <div
-      className={`${"order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3 bg-emerald-200 mt-[14px]"} p-5 rounded-2xl`}
+      className={`${"order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3 bg-unnamed-color-ffffff mt-[14px]"} p-5 rounded-2xl`}
     >
       <h3 className="font-manrope font-extrabold text-2xl leading-[33px] text-unnamed-color-072635">
         Clock
--- src/components/NewsFeed.tsx
+++ src/components/NewsFeed.tsx
@@ -1,7 +1,7 @@
 const NewsFeed = () => {
   return (
     <div
-      className={`${"order-2 lg:order-2 lg:col-span-8 xl:col-span-6 bg-emerald-400"} p-5 rounded-2xl`}
+      className={`${"order-2 lg:order-2 lg:col-span-8 xl:col-span-6 bg-unnamed-color-ffffff"} p-5 rounded-2xl`}
     >
       <h3 className="font-manrope font-extrabold text-2xl leading-[33px] text-unnamed-color-072635">
         News feed
