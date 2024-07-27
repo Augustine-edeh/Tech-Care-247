4c1dd289cf238fc3985f02773daf36964d3a7000
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-07-27 12:30:41 +0100

    chore(component rename): rename `News` to `NewsPage` for better clarity
    - This commit renames the News component to NewsPage to improve clarity and maintain consistency in the codebase. The new name more accurately reflects the purpose of the component, which is to serve as a dedicated page for displaying news.

---
+++ src/app/(pages)/news/page.tsx
@@ -0,0 +1,29 @@
+import Header from "@/components/Header";
+import Main from "@/components/Main";
+import PatientDiagnosisInfo from "@/components/PatientDiagnosisInfo";
+import PatientProfile from "@/components/PatientProfile";
+import PatientsList from "@/components/PatientsList";
+
+const NewsPage = () => {
+  return (
+    <>
+      <div
+        className={`${"order-2 lg:order-2 lg:col-span-8 xl:col-span-6 bg-emerald-400"} p-5 rounded-2xl`}
+      >
+        <h3 className="font-manrope font-extrabold text-2xl leading-[33px] text-unnamed-color-072635">
+          News feed
+        </h3>
+      </div>
+
+      <div
+        className={`${"order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3 bg-emerald-200 mt-[14px]"} p-5 rounded-2xl`}
+      >
+        <h3 className="font-manrope font-extrabold text-2xl leading-[33px] text-unnamed-color-072635">
+          Clock
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default NewsPage;
