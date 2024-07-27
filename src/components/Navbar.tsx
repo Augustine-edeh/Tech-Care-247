a2451d1a9f7e30ca0320a8853d2775a57666603e
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-07-27 17:49:34 +0100

    fix(Navbar): improve active link styling for better user navigation
    - Enhanced the active link styling logic using `clsx` to correctly highlight the current page in the navigation.
    - Ensured that the `pathname` is accurately checked against each link to apply the active styles.
    - Installed `clsx` for conditional class name management.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,106 +1,77 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 
 const Navbar = () => {
+  const pathname = usePathname();
+
   return (
     <nav className="nav hidden lg:flex items-center justify-between w-[663px] h-[63px]">
-      <Link
-        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
-        href="/"
-      >
-        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
-          <Image
-            src="/home_FILL0_wght300_GRAD0_opsz24.svg"
-            width={16}
-            height={17}
-            alt="home"
-          />
-
-          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-            Overview
-          </p>
-        </div>
-      </Link>
-
-      <Link
-        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
-        href="/"
-      >
-        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
-          <Image
-            src="/group_FILL0_wght300_GRAD0_opsz24.svg"
-            width={24}
-            height={17}
-            alt="home"
-          />
-          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-            Patients
-          </p>
-        </div>
-      </Link>
-
-      <Link
-        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
-        href="/"
-      >
-        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
-          <Image
-            src="/calendar_today_FILL0_wght300_GRAD0_opsz24.svg"
-            width={15}
-            height={17}
-            alt="home"
-          />
-          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-            Schedule
-          </p>
-        </div>
-      </Link>
-
-      <Link
-        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
-        href="/"
-      >
-        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
-          <Image
-            src="/group_FILL0_wght300_GRAD0_opsz24.svg"
-            width={19}
-            height={17}
-            alt="home"
-          />
-          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-            Message
-          </p>
-        </div>
-      </Link>
-
-      <Link
-        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
-        href="/"
-      >
-        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
-          <Image
-            src="/credit_card_FILL0_wght300_GRAD0_opsz24.svg"
-            width={22}
-            height={17}
-            alt="home"
-          />
-          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-            Transactions
-          </p>
-        </div>
-      </Link>
-
-      <Link
-        className="flex justify-center items-center gap-[9px] h-[41px]  focus:bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]"
-        href="news"
-      >
-        <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
-          <Image src="/news.svg" width={200} height={200} alt="news" />
-          <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
-            News
-          </p>
-        </div>
-      </Link>
+      {[
+        {
+          href: "/",
+          icon: "/home_FILL0_wght300_GRAD0_opsz24.svg",
+          label: "Overview",
+          iconSize: { width: 16, height: 17 },
+        },
+        {
+          href: "/patients",
+          icon: "/group_FILL0_wght300_GRAD0_opsz24.svg",
+          label: "Patients",
+          iconSize: { width: 24, height: 17 },
+        },
+        {
+          href: "/schedule",
+          icon: "/calendar_today_FILL0_wght300_GRAD0_opsz24.svg",
+          label: "Schedule",
+          iconSize: { width: 15, height: 17 },
+        },
+        {
+          href: "/message",
+          icon: "/group_FILL0_wght300_GRAD0_opsz24.svg",
+          label: "Message",
+          iconSize: { width: 19, height: 17 },
+        },
+        {
+          href: "/transactions",
+          icon: "/credit_card_FILL0_wght300_GRAD0_opsz24.svg",
+          label: "Transactions",
+          iconSize: { width: 22, height: 17 },
+        },
+        {
+          href: "/news",
+          icon: "/news.svg",
+          label: "News",
+          iconSize: { width: 16, height: 17 },
+        },
+      ].map((link) => (
+        <Link
+          key={link.href}
+          className={clsx(
+            "flex justify-center items-center gap-[9px] h-[41px] hover:bg-unnamed-activestate-bg-2 outline-none rounded-[41px]",
+            {
+              "bg-unnamed-activestate-bg-1 hover:bg-unnamed-activestate-bg-1":
+                pathname === link.href,
+            }
+          )}
+          href={link.href}
+        >
+          <div className="h-[19px] mx-5 flex justify-center gap-x-[9px]">
+            <Image
+              src={link.icon}
+              width={link.iconSize?.width || 16}
+              height={link.iconSize?.height || 17}
+              alt={link.label.toLowerCase()}
+            />
+            <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
+              {link.label}
+            </p>
+          </div>
+        </Link>
+      ))}
     </nav>
   );
 };
