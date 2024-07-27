9ccb18fb71962d3525aceded41a91b68a478622a
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-07-27 17:46:21 +0100

    remove dead code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,6 @@ import Navbar from "./Navbar";
 import User from "./User";
 import Logo from "./Logo";
 
-Image;
 const Header = () => {
   return (
     <header className="flex items-center justify-between px-[20px] md:px-[33px] h-[72px] bg-unnamed-color-ffffff rounded-[70px]">
