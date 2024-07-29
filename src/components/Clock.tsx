98cb7092af33f4cc4bb709c7ed126c6eed63088f
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-07-29 23:59:37 +0100

    feat: Add functional Clock feature to display current time
    - Implemented Clock component using React hooks (useState and useEffect)
    - Clock component updates every second to show the current time
    - Styled the Clock component to match existing design

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,4 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+
 const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div
       className={`${"order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3 bg-unnamed-color-ffffff mt-[14px]"} p-5 rounded-2xl`}
@@ -6,6 +19,9 @@ const Clock = () => {
       <h3 className="font-manrope font-extrabold text-2xl leading-[33px] text-unnamed-color-072635">
         Clock
       </h3>
+      <p className="font-manrope text-xl leading-[33px] text-unnamed-color-072635">
+        {time.toLocaleTimeString()}
+      </p>
     </div>
   );
 };
