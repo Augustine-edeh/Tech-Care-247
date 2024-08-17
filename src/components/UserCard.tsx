00d4a3bc3a0483671f7495a7b81839191f5488ff
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-08-17 03:02:38 +0100

    refactor(UserCard): rename component & modularize component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import SettingsIcon from "./SettingsIcon";
 import MenuIcon from "./MenuIcon";
 
-const User = () => {
+const UserCard = () => {
   return (
-    <div className="flex items-center w-[241p] h-[44px] divide-x-2">
+    <div className="flex items-center w-[241p] h-[44px]">
       <section className="flex pr-3 gap-x-2 cursor-pointer">
         <Image
-          src="/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc@2x.png"
+          src="/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc.png"
           width={44}
           height={44}
           alt="doctor"
@@ -23,12 +23,12 @@ const User = () => {
         </div>
       </section>
 
-      <section className="flex gap-x-3 pl-3">
+      {/* <section className="flex gap-x-3 pl-3">
         <SettingsIcon />
         <MenuIcon />
-      </section>
+      </section> */}
     </div>
   );
 };
 
-export default User;
+export default UserCard;
