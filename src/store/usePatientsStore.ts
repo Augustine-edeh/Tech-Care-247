f05d468d0ae1a295bd98105785a677e50dbe55c2
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-08-17 20:43:22 +0100

    fix: resolve deployment failure due to implicit 'any' type in Zustand store

--- src/store/usePatientsStore.ts
+++ src/store/usePatientsStore.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 
-export const useStore = create((set) => ({
+interface PatientsStore {
+  patientsData: any; // TODO: Replace `any` with actual type later
+  setPatientsData: (data: any) => void; // TODO: Replace `any` with the actual type of your patients data later
+}
+
+export const useStore = create<PatientsStore>((set) => ({
   patientsData: null,
   setPatientsData: (data) => set({ patientsData: data }),
 }));
