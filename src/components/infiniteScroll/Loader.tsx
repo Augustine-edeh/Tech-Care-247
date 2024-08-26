c708228be8505f0a031eacba9c63f617ee3812c6
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-08-26 03:55:52 +0100

    chore: modify loader UI for `NewsFeed` component

--- src/components/infiniteScroll/Loader.tsx
+++ src/components/infiniteScroll/Loader.tsx
@@ -2,17 +2,35 @@ const Loader = () => {
   return (
     <div className="flex flex-col items-center justify-center p-4">
       <svg
-        className="animate-spin h-8 w-8 text-blue-600"
+        className="animate-spin h-8 w-8"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
       >
+        <defs>
+          <linearGradient
+            id="deepBlueSkyBlueGradient"
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="100%"
+          >
+            <stop
+              offset="0%"
+              style={{ stopColor: "#1E3A8A", stopOpacity: 1 }} // Deep Blue
+            />
+            <stop
+              offset="100%"
+              style={{ stopColor: "#60A5FA", stopOpacity: 1 }} // Sky Blue
+            />
+          </linearGradient>
+        </defs>
         <circle
           cx="12"
           cy="12"
           r="10"
-          stroke="currentColor"
+          stroke="url(#deepBlueSkyBlueGradient)"
           strokeWidth="4"
           fill="none"
         />
