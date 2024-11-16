eac253c46ded00efe855d8a5fde56bbbc17f69fe
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-16 16:46:55 +0100

    remove dead style className

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import PatientsList from "./PatientsList";
 
 const Main = () => {
   return (
-    <main className="grid grid-cols-1 xl:grid-cols-12 gap-8 lg:border-red-600 lg:grid-cols-12 -[1195px]">
+    <main className="grid grid-cols-1 xl:grid-cols-12 gap-8 lg:border-red-600 lg:grid-cols-12">
       <PatientsList className={"hidden order-1  xl:block xl:col-span-3"} />
       <PatientDiagnosisInfo
         className={"order-2 lg:order-2 lg:col-span-8 xl:col-span-6"}
