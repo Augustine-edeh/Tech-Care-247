d5e309d24ea249dd0ed8be5a516d46a4dd05cdb3
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-17 10:26:12 +0100

    add minor changes to `Main` & `PatientsList`

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -21,30 +21,45 @@ const PatientsList = ({ className }: ClassNameType) => {
   }, [fetchPatientsData]);
 
   return (
-    <section
-      className={`${className} bg-unnamed-color-ffffff mt-[14px] rounded-[16px] p-5`}
+    <div
+      className={`${className}flex flex- col flex-1 bg-yellow-200 -unnamed-color-ffffff xl:rounded-[16px] p-5 w-ful`}
     >
       <SearchPatients className="mb-10 mr-5" />
 
-      {isLoading ? (
-        <div className="flex justify-center items-center h-full">
-          <p>Loading...</p>
-        </div>
-      ) : (
-        <ul className="patient-list flex flex-col overflow-y-scroll h-[1076px] overflow-x-hidden w-full">
-          {patients?.map((patient, index) => (
-            <li key={index} onClick={() => setSelectedPatient(patient)}>
-              <PatientCard
-                profile_picture={patient.profile_picture}
-                name={patient.name}
-                gender={patient.gender}
-                age={patient.age}
-              />
-            </li>
-          ))}
-        </ul>
-      )}
-    </section>
+      <div className="flex-1 overflow-y-auto bg-red-500">
+        {isLoading ? (
+          <div className="flex justify-center items-center h-full">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <p> YeA!!!!!!!</p>
+        )}
+      </div>
+
+      {/* {
+        isLoading ? (
+          <div className="flex justify-center items-center h-full">
+            <p>Loading...</p>
+          </div>
+        ) : (
+          ""
+        )
+        // (
+        //   <ul className="patient-list flex flex-col overflow-y-scroll h- [1076px] w-full">
+        //     {patients?.map((patient, index) => (
+        //       <li key={index} onClick={() => setSelectedPatient(patient)}>
+        //         <PatientCard
+        //           profile_picture={patient.profile_picture}
+        //           name={patient.name}
+        //           gender={patient.gender}
+        //           age={patient.age}
+        //         />
+        //       </li>
+        //     ))}
+        //   </ul>
+        // )
+      } */}
+    </div>
   );
 };
 
