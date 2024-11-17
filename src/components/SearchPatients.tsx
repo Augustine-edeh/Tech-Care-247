ce9f3d3f3c639f1b4f87ec3a09e02b4ca71842dc
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-17 11:46:11 +0100

    chore: modify `SearchPatients`

--- src/components/SearchPatients.tsx
+++ src/components/SearchPatients.tsx
@@ -1,17 +1,27 @@
-import Image from "next/image";
+"use client";
 
-const SearchPatients = ({ className }: { className: string }) => {
+import { useState } from "react";
+import { ClassNameType } from "../../types/Ui";
+import { Search } from "lucide-react";
+
+const SearchPatients = ({ className }: ClassNameType) => {
+  const [isSearcheClicked, setIsSearcheClicked] = useState<boolean>(false);
+
+  const showSearchInput = () => setIsSearcheClicked((prev) => !prev);
+
   return (
-    <div className={`${className} flex justify-between`}>
-      <h3 className="font-manrope font-extrabold text-xl leading-[33px] text-unnamed-color-072635 text-left">
-        Patients
-      </h3>
-      <Image
-        src="/search_FILL0_wght300_GRAD0_opsz24.svg"
-        width={18}
-        height={18}
-        alt="search"
-      />
+    <div className={`${className} flex justify-between items-center`}>
+      <div className="h-8">
+        {isSearcheClicked ? (
+          <input type="text" className="h-full" />
+        ) : (
+          <h3 className="font-manrope font-extrabold text-xl leading-8 text-unnamed-color-072635">
+            Patients
+          </h3>
+        )}
+      </div>
+
+      <Search onClick={showSearchInput} className="cursor-pointer" />
     </div>
   );
 };
