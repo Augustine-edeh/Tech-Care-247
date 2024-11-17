3df498da3048f0cded461e0694fcb858e956d3ec
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-17 10:59:47 +0100

    chore: remove dead code

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,49 +10,9 @@ const Main = () => {
 
   return (
     <main className="flex flex-1 bg-blue-200 overflow-y-auto">
-      {/* {activeTab === "patients" && (
-        <ul>
-          {Array.from({ length: 50 }, () => "patient").map((el, index) => (
-            <li key={index}>{`${el} ${++index}`}</li>
-          ))}
-        </ul>
-      )} */}
-
       {activeTab === "patients" && (
         <PatientsList className={"order-1 xl:block xl:col-span-3"} />
       )}
-
-      {/* <div className="xl: hidden">
-        {activeTab === "patients" && (
-          <PatientsList className={"order-1 xl:block xl:col-span-3"} />
-        )}
-
-        {activeTab === "overview" && (
-          <PatientDiagnosisInfo
-            className={"order-2 lg:order-2 lg:col-span-8 xl:col-span-6"}
-          />
-        )}
-
-        {activeTab === "diagnosis" && (
-          <PatientProfile
-            className={
-              "order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3"
-            }
-          />
-        )}
-      </div>
-
-      <div className="hidden xl:grid grid-cols-1 xl:grid-cols-12 gap-8 lg:border-red-600 lg:grid-cols-12">
-        <PatientsList className={"hidden order-1 xl:block xl:col-span-3"} />
-        <PatientDiagnosisInfo
-          className={"order-2 lg:order-2 lg:col-span-8 xl:col-span-6"}
-        />
-        <PatientProfile
-          className={
-            "order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3"
-          }
-        />
-      </div> */}
     </main>
   );
 };
