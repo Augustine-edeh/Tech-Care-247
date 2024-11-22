bb0776a8cf4ea71faf3ded9064d81dfcdb76017b
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-23 00:08:18 +0100

    feat(pages): add placeholder content for all other pages

---
+++ src/app/(pages)/transactions/page.tsx
@@ -0,0 +1,14 @@
+import BottomNav from "@/components/BottomNav";
+import Header from "@/components/layout/Header";
+import Main from "@/components/layout/Main";
+const TransactionPage = () => {
+  return (
+    <div className="h-screen flex flex-col lg:gap-y-5 lg:p-5 bg-unnamed-page-bg">
+      <Header />
+      <Main />
+      <BottomNav />
+    </div>
+  );
+};
+
+export default TransactionPage;
