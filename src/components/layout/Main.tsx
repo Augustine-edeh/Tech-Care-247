8ecc923e69762de554a4050a409c608cf8875809
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-27 00:30:15 +0100

    feat: implement movile layout for `NewsFeed` page

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -1,34 +1,43 @@
 "use client";
 
+import { useRouter, usePathname } from "next/navigation";
 import useActiveTabStore from "@/store/useActiveTabStore";
 import PatientsList from "../patients/PatientsList";
 import PatientDiagnosisInfo from "../patients/PatientDiagnosisInfo";
 import PatientProfile from "../patients/PatientProfile";
+import NewsFeed from "../news/NewsFeed";
 
 const Main = () => {
   const activeTab = useActiveTabStore((state) => state.activeTab);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  // Synchronize active tab with root path
+  const isRootRoute = pathname === "/";
 
   return (
     <main className="flex flex-1 h-screen overflow-hidden">
-      {/* Mobile view */}
+      {/* Mobile View */}
       <div className="w-full xl:hidden">
-        {activeTab === "patients" && <PatientsList />}
+        {isRootRoute && activeTab === "patients" && <PatientsList />}
 
-        {activeTab === "overview" && <PatientDiagnosisInfo />}
+        {isRootRoute && activeTab === "overview" && <PatientDiagnosisInfo />}
 
-        {activeTab === "diagnosis" && <PatientProfile />}
+        {isRootRoute && activeTab === "diagnosis" && <PatientProfile />}
+
+        {/* {isRootRoute && activeTab === "news" && <NewsFeed />} */}
       </div>
 
       {/* Desktop View */}
       <div className="hidden xl:grid grid-cols-12 gap-10 w-full">
         <div className="col-span-3 overflow-hidden rounded-2xl bg-unnamed-color-ffffff">
-          <PatientsList />
+          {isRootRoute && <PatientsList />}
         </div>
         <div className="col-span-6 overflow-hidden rounded-2xl">
-          <PatientDiagnosisInfo />
+          {isRootRoute && <PatientDiagnosisInfo />}
         </div>
         <div className="col-span-3 overflow-hidden rounded-2xl">
-          <PatientProfile />
+          {isRootRoute && <PatientProfile />}
         </div>
       </div>
     </main>
--- src/components/ui/Clock.tsx
+++ src/components/ui/Clock.tsx
@@ -4,16 +4,18 @@ import "react-clock/dist/Clock.css";
 
 const Clock = () => {
   return (
-    <div className="bg-unnamed-color-ffffff h-full p-5">
-      <h3 className="font-manrope font-extrabold text-2xl leading-8 text-unnamed-color-072635 mb-10">
-        Clock
-      </h3>
+    <div className="bg-unnamed-color-ffffff xl:h-full p-5">
+      <div className="flex justify-between">
+        <h3 className="font-manrope font-extrabold text-2xl leading-8 text-unnamed-color-072635 xl:mb-10">
+          Clock
+        </h3>
 
-      <section className="xl:hidden">
-        <DigitalClock />
-      </section>
+        <section className="xl:hidden">
+          <DigitalClock />
+        </section>
+      </div>
 
-      <section className="hidden lg:flex justify-center items-center">
+      <section className="hidden xl:flex justify-center items-center">
         <AnalogClock />
       </section>
     </div>
