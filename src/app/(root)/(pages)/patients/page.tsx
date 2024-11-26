d128ccd961d5533575875bd0432c0e1e265ad760
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-26 19:16:11 +0100

    chore: remove dead code

--- src/app/(root)/(pages)/patients/page.tsx
+++ src/app/(root)/(pages)/patients/page.tsx
@@ -1,13 +1,6 @@
-import BottomNav from "@/components/BottomNav";
-import Header from "@/components/layout/Header";
 import Main from "@/components/layout/Main";
 const PatientsPage = () => {
   return (
-    // <div className="h-screen flex flex-col lg:gap-y-5 lg:p-5 bg-unnamed-page-bg">
-    //   <Header />
-    //   <Main />
-    //   <BottomNav />
-    // </div>
     <>
       <Main />
     </>
