cf3c4c8dd91d504354f1c89d715df935efbaddf8
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-26 18:07:55 +0100

    refactor: replace <a> tag with Next.js <Link> component for improved navigation and updated patient card behavior.
    - This commit improves PatientCard accessibility.

--- src/components/patients/PatientCard.tsx
+++ src/components/patients/PatientCard.tsx
@@ -1,5 +1,6 @@
 import useNavigateToOverviewTab from "@/hooks/useNavigateToOverviewTab";
 import Image from "next/image";
+import Link from "next/link";
 
 type patientCardType = {
   profile_picture: string;
@@ -16,8 +17,9 @@ const PatientCard = ({
 }: patientCardType) => {
   const navigateToOverviewTab = useNavigateToOverviewTab();
   return (
-    <a
+    <Link
       href="#"
+      onClick={navigateToOverviewTab}
       className="flex items-center h- 20 w-[353px bg-unnamed-color-ffffff outline-none focus:bg-unnamed-activestate-bg-2 hover:bg-unnamed-activestate-bg-2/40"
     >
       <div className="flex justify-between w-[317px w-full h-[48px] m-4">
@@ -47,7 +49,7 @@ const PatientCard = ({
           alt="more info"
         />
       </div>
-    </a>
+    </Link>
   );
 };
 
