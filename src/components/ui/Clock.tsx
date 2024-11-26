acc935568665b813909207ee811db074ebed89fd
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-26 19:19:06 +0100

    chore: modify layout for `NewsFeed`

--- src/components/ui/Clock.tsx
+++ src/components/ui/Clock.tsx
@@ -4,18 +4,16 @@ import "react-clock/dist/Clock.css";
 
 const Clock = () => {
   return (
-    <div
-      className={`${"order-1 lg:order-3 lg:col-span-4 xl:order-3 xl:col-span-3 bg-unnamed-color-ffffff mt-[14px]"} p-5 rounded-2xl`}
-    >
-      <h3 className="font-manrope font-extrabold text-2xl leading-[33px] text-unnamed-color-072635 mb-10">
+    <div className="bg-green-400 h-full p-5">
+      <h3 className="font-manrope font-extrabold text-2xl leading-8 text-unnamed-color-072635 mb-10">
         Clock
       </h3>
 
-      <section className="lg:hidden">
+      <section className="xl:hidden">
         <DigitalClock />
       </section>
 
-      <section className="hidden  lg:flex justify-center items-center">
+      <section className="hidden lg:flex justify-center items-center">
         <AnalogClock />
       </section>
     </div>
