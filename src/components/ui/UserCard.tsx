2ef577d4398661eff0e97d1681ae76b6c4e0e131
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-28 00:56:12 +0100

    chore: modify styles  for `UserCard`

--- src/components/ui/UserCard.tsx
+++ src/components/ui/UserCard.tsx
@@ -2,16 +2,17 @@ import Image from "next/image";
 
 const UserCard = () => {
   return (
-    <div className="flex items-center w-[241p] h-[44px]">
-      <section className="flex pr-3 gap-x-2 cursor-pointer">
+    <div className="flex items-center h- [44px] bg-red -500">
+      <section className="flex items-center pr-3 gap-x-2 cursor-pointer">
         <Image
           src="/senior-woman-doctor-and-portrait-smile-for-health-2023-11-27-05-18-16-utc.png"
           width={44}
           height={44}
           alt="doctor"
+          className="xl:size-14 rounded-full"
         />
 
-        <div className="hidden md:flex lg:hidden xl:flex flex-col justify-center w-[129] h-[38p]">
+        <div className="hidden xl:flex flex-col justify-center">
           <p className="font-manrope font-bold text-sm leading-[19px] text-unnamed-color-072635 text-left">
             Dr. Jose Simmons
           </p>
