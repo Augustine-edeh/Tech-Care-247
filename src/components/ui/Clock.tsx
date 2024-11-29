0947db3da1dc7873712a3d12615d5c865e7889aa
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-11-29 03:50:40 +0100

    fix: fix route layout and handling  for `Schedule`

--- src/components/ui/Clock.tsx
+++ src/components/ui/Clock.tsx
@@ -1,10 +1,12 @@
 import AnalogClock from "./AnalogClock";
 import DigitalClock from "./DigitalClock";
 import "react-clock/dist/Clock.css";
+import { ClassNameType } from "../../../types/Ui";
+import clsx from "clsx";
 
-const Clock = () => {
+const Clock = ({ className }: ClassNameType) => {
   return (
-    <div className="bg-unnamed-color-ffffff xl:h-full p-5">
+    <div className={clsx("bg-unnamed-color-ffffff xl:h-full p-5", className)}>
       <div className="flex justify-between">
         <h3 className="font-manrope font-extrabold text-2xl leading-8 text-unnamed-color-072635 xl:mb-10">
           Clock
