d8cf9f13038e835f4e8a58e965fcc80aac1991d1
Augustine Edeh <info.augustinesedeh@gmail.com>
2025-04-02 03:13:47 +0100

    feat(appointments): enhance Appointments component with new layout and icons

--- src/components/Appointments.tsx
+++ src/components/Appointments.tsx
@@ -1,3 +1,5 @@
+import { Calendar, TrendingUp } from "lucide-react";
+
 const Appointments = ({ className = "" }) => {
   const appointments = [
     {
@@ -30,7 +32,7 @@ const Appointments = ({ className = "" }) => {
     <div className={`flex flex-col rounded-2xl border p-5 ${className}`}>
       <h4 className="text-2xl">Appointments</h4>
 
-      <ul className="flex-1 list-disc pl-5 space-y-3 overflow-y-auto">
+      {/* <ul className="flex-1 list-disc pl-5 space-y-3 overflow-y-auto">
         {appointments.map((appointment, index) => (
           <li key={index} className="bg-white/10 p-3 rounded-lg">
             <p className="font-semibold text-lg">{appointment.type}</p>
@@ -39,6 +41,34 @@ const Appointments = ({ className = "" }) => {
             <p className="text-sm opacity-90">👨‍⚕️ {appointment.doctor}</p>
           </li>
         ))}
+      </ul> */}
+
+      <ul className="flex flex-col justify-between">
+        <li>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <Calendar />
+              <div className="">
+                <p className="font-bold">Consultaion</p>
+                <p className="opacity-70">Scheduled</p>
+              </div>
+            </div>
+            <span>10:00 AM</span>
+          </div>
+        </li>
+
+        <li>
+          <div className="flex justify-between">
+            <div className="flex items-center gap-2">
+              <TrendingUp />
+              <div className="">
+                <p className="font-bold">Follow-up</p>
+                <p className="opacity-70">Past</p>
+              </div>
+            </div>
+            <span>Feb 20 2024</span>
+          </div>
+        </li>
       </ul>
     </div>
   );
